cab4a5974fff4173c01aa1fe14f6e864256b47fa
NoorulAmeenJ <noorularafat786@gmail.com>
2022-12-22 20:46:24 +0530

    day_1_completed

---
+++ ameen.js
@@ -0,0 +1,20 @@
+
+let a = document.createElement("h1");
+a.innerText='hi i am noorul';
+a.style.border='1px solid red';
+a.style.color='violet';
+a.style.backgroundColor='pink';
+a.setAttribute("class","cla")
+
+var b= document.getElementById("noorula");
+b.innerText="sanjay";
+var c= document.querySelector('#ameen');
+c.innerText="yadav";
+var d= document.querySelector('.noorul');
+d.innerText="gowda";
+
+
+document.body.append(a);
+console.log(document);
+
+
